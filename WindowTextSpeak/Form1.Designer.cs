539079228652226a1869c0bcfd04bc440a821b2d
Hoanglh8 <62165784+Hoanglh8@users.noreply.github.com>
2023-12-26 15:47:21 +0700

    update

--- WindowTextSpeak/Form1.Designer.cs
+++ WindowTextSpeak/Form1.Designer.cs
@@ -43,14 +43,16 @@
             this.label6 = new System.Windows.Forms.Label();
             this.buttonRead = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(7, 167);
+            this.richTextBox1.Location = new System.Drawing.Point(7, 182);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(686, 206);
+            this.richTextBox1.Size = new System.Drawing.Size(686, 191);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
@@ -139,7 +141,7 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.White;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(3, 140);
+            this.label5.Location = new System.Drawing.Point(3, 155);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(316, 24);
             this.label5.TabIndex = 9;
@@ -186,12 +188,45 @@
             this.pictureBox1.TabIndex = 13;
             this.pictureBox1.TabStop = false;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "HN - Mai Phương",
+            "HN - Thanh Long",
+            "HN - Mạnh Dũng",
+            "HN - Phú Thăng",
+            "HN - Ngọc Huyền",
+            "Huế - Duy Phương",
+            "Huế - Hương Giang",
+            "SG - Thảo Trinh",
+            "SG - Minh Hoàng",
+            "SG - Trung Kiên",
+            "SG - Lan Trinh"});
+            this.comboBox1.Location = new System.Drawing.Point(243, 120);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(228, 21);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(3, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Chọn giọng đọc";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(708, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.buttonRead);
             this.Controls.Add(this.label6);
@@ -230,6 +265,8 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button buttonRead;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
